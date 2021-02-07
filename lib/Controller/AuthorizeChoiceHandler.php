0a11aabb2ee2073f580b296dac2bc706aefecc84
Sebastiano Degan <sebastiano@linux.fritz.box>
2021-02-07 21:14:12 +0100

    removed unused imports

--- lib/Controller/AuthorizeChoiceHandler.php
+++ lib/Controller/AuthorizeChoiceHandler.php
@@ -5,19 +5,13 @@ namespace SimpleSAML\Module\oauth2\Controller;
 
 
 use League\OAuth2\Server\AuthorizationServer;
-use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
-use League\OAuth2\Server\Repositories\UserRepositoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use SimpleSAML\Configuration;
 use SimpleSAML\Error\BadRequest;
-use SimpleSAML\Module;
 use SimpleSAML\Module\oauth2\AuthRequestSerializer;
-use SimpleSAML\Module\oauth2\Entity\UserEntity;
 use SimpleSAML\Module\oauth2\Form\AuthorizeForm;
-use SimpleSAML\XHTML\Template;
 
 class AuthorizeChoiceHandler implements RequestHandlerInterface
 {
@@ -49,7 +43,8 @@ class AuthorizeChoiceHandler implements RequestHandlerInterface
         return $this->authorizationServer->completeAuthorizationRequest($authorizationRequest, $this->newResponse());
     }
 
-    private function newResponse() {
+    private function newResponse()
+    {
         return $this->responseFactory->createResponse();
     }
 }
