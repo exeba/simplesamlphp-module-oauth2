5c94d179e34373e3943700aa21089869df7059d3
Sebastiano Degan <sebastiano@localhost.localdomain>
2022-11-08 19:20:17 +0100

    removed unused files

--- example/files/simplesamlphp/cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-require_once 'vendor/autoload.php';
-
-$entityManager = SimpleSAML\Module\oauth2\Repositories\EntityManagerProvider::getEntityManager();
-
-return ConsoleRunner::createHelperSet($entityManager);
