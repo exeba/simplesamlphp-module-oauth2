d5b7e531d2d6c09c6b5b8033c955220ccdd40218
Sebastiano Degan <sebastiano@localhost.localdomain>
2022-11-13 10:51:01 +0100

    doctrine: fixed proxy dir + isolated test env

--- src/Factories/EntityManagerFactory.php
+++ src/Factories/EntityManagerFactory.php
@@ -2,23 +2,34 @@
 
 namespace SimpleSAML\Module\oauth2\Factories;
 
+use DAMA\DoctrineTestBundle\Doctrine\DBAL\Middleware;
+use DAMA\DoctrineTestBundle\Doctrine\DBAL\PostConnectEventListener;
 use Doctrine\Common\EventManager;
+use Doctrine\DBAL\Events as DBALEvents;
 use Doctrine\ORM\Configuration as ORMConfiguration;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Events;
+use Doctrine\ORM\Events as ORMEvents;
 use Doctrine\ORM\ORMSetup;
 use SimpleSAML\Configuration as SimpleSAMLConfiguration;
 use SimpleSAML\Module\oauth2\DoctrineExtensions\TablePrefix;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class EntityManagerFactory
 {
     private $moduleConfig;
     private $isProductionMode;
+    private $environment;
+    private $proxyDir;
 
-    public function __construct(SimpleSAMLConfiguration $moduleConfig, bool $isProductionMode)
+    public function __construct(
+        SimpleSAMLConfiguration $moduleConfig,
+        bool $isProductionMode,
+        KernelInterface $kernel)
     {
         $this->moduleConfig = $moduleConfig;
         $this->isProductionMode = $isProductionMode;
+        $this->environment = $kernel->getEnvironment();
+        $this->proxyDir = $kernel->getCacheDir().DIRECTORY_SEPARATOR.'doctrine_proxies';
     }
 
     public function buildEntityManager()
@@ -39,14 +50,21 @@ class EntityManagerFactory
     private function buildMetadataConfiguration(): ORMConfiguration
     {
         $paths = [__DIR__.'/../../doctrine-mappings'];
+        $config = ORMSetup::createXMLMetadataConfiguration($paths, !$this->isProductionMode, $this->proxyDir);
+        if ($this->isTestEnv()) {
+            $config->setMiddlewares([new Middleware()]);
+        }
 
-        return ORMSetup::createXMLMetadataConfiguration($paths, !$this->isProductionMode);
+        return $config;
     }
 
     private function buildEventManager(): EventManager
     {
         $evm = new EventManager();
-        $evm->addEventListener(Events::loadClassMetadata, $this->buildTablePrefix());
+        $evm->addEventListener(ORMEvents::loadClassMetadata, $this->buildTablePrefix());
+        if ($this->isTestEnv()) {
+            $evm->addEventListener(DBAlEvents::postConnect, new PostConnectEventListener());
+        }
 
         return $evm;
     }
@@ -56,5 +74,10 @@ class EntityManagerFactory
         $prefix = $this->moduleConfig->getOptionalString('oauth2.dbal.prefix', '');
 
         return new TablePrefix($prefix);
+    }
+
+    private function isTestEnv()
+    {
+        return 'test' === $this->environment;
     }
 }
